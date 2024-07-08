0fd92ef96938f19ef31070ec51b5f632e65aacf0
haru-15 <m1m24a33@oit.ac.jp>
2024-07-08 12:10:28 +0900

    中村変更3

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -5,10 +5,9 @@ public class Monster {
   String name;
   int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
 
-  Monster() {
-    Random random = new Random();
-    this.name = this.summonMonster(random.nextInt(5));
-    this.rare = random.nextInt(5);
+  Monster(int number) {
+    this.name = this.summonMonster(number);
+    this.rare = number;
   }
 
   String summonMonster(int mnumber) {
--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -4,15 +4,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MonsterTest {
-    @Test public void appHasAsummonmonster() {
-        App classUnderTest = new Monster();
-        assertNotNull("スライム", classUnderTest.summonMonster(1));
-    }
 
     @Test public void appHasAtoString() {
-        App classUnderTest = new Monster();
-        classUnderTest.name = "スライム";
-        classUnderTest.rare = 2;
-        assertNotNull("スライム:レア度[2]", classUnderTest.toString());
+        App classUnderTest = new Monster(1);
+
+        assertNotNull("スライム:レア度[1]", classUnderTest.toString());
     }
 }
